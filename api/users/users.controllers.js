e406ca5d77202e6ff0a3fa5a26a945b452f23909
Fahad <fahhod.ahmad@gmail.com>
2023-06-29 05:32:33 +0300

    not finished

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,26 +1,48 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_EXPIRATION, SECRET_KEY } = require("../../config/keys");
 
-exports.signin = async (req, res) => {
+const generateToken = (user) => {
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, {
+    expiresIn: JWT_EXPIRATION,
+  });
+  return token;
+};
+
+exports.signin = async (req, res, next) => {
   try {
+    const { user } = req;
+    const token = generateToken(user);
+    res.status(201).json({ token });
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
 
-exports.signup = async (req, res) => {
+exports.signup = async (req, res, next) => {
+  const { password } = req.body;
+  const saltRounds = 10;
   try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    req.body.password = hashedPassword;
     const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    const token = generateToken(newUser);
+    res.status(201).json({ token });
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
 
-exports.getUsers = async (req, res) => {
+exports.getUsers = async (req, res, next) => {
   try {
-    const users = await User.find().populate('urls');
+    const users = await User.find().populate("urls");
     res.status(201).json(users);
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
---
+++ middlewares/passport.js
@@ -0,0 +1,45 @@
+const { LocalStrategy, JWTStrategy } = require("passport-local").Strategy;
+const { fromAuthHeaderAsBearerToken } = require("passport-jwt").ExtractJwt;
+const User = require("../db/models/User");
+const bcrypt = require("bcrypt");
+const { SECRET_KEY } = require("../config/keys");
+
+exports.localStrategy = new LocalStrategy(
+  { usernameField: "username" },
+  async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      const passwordsMatch = user
+        ? bcrypt.compare(password, user.password)
+        : false;
+      if (passwordsMatch) {
+        return done(null, user);
+      }
+      return done(null, false);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
+
+exports.jwtStrategy = new JWTStrategy(
+  {
+    jwtFromRequest: fromAuthHeaderAsBearerToken(),
+    secretOrKey: SECRET_KEY,
+  },
+  async (jwtPayLoad, done) => {
+    if (Date.now() > jwtPayLoad.exp * 1000) {
+      return done(null, false);
+    }
+    try {
+      const user = await User.findById(jwtPayLoad._id);
+
+      if (!user) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+);
